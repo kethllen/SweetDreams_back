a1d25cc4d1aa8f5281f65bb315a71af33d4a38f5
kethllen <kety_andrey@hotmail.com>
2022-02-09 10:26:46 -0300

    feat: Init routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import authRouter from './authRouter.js';
-import dataRouter from './dataRouter.js';
+import { Router } from "express";
+import authRouter from "./authRouter.js";
+import productsRouter from "./productsRouter.js";
 
 const router = Router();
 router.use(authRouter);
-router.use(dataRouter);
+router.use(productsRouter);
 
 export default router;
