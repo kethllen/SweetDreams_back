50b4c22312a564947563370ff38384bc0560379e
kethllen <kety_andrey@hotmail.com>
2022-02-11 13:44:40 -0300

    headers

--- src/middlewares/validaTokenMiddleware.js
+++ src/middlewares/validaTokenMiddleware.js
@@ -3,8 +3,8 @@ import { ObjectId } from "mongodb";
 
 export default async function validateToken(req, res, next) {
   try {
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
+    const { Authorization } = req.headers;
+    const token = Authorization?.replace("Bearer ", "");
     if (!token) {
       return res.sendStatus(401);
     }
