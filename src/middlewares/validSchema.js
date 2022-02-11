f2f9701602bf1d83fd851b0e70b635e9ae2054c5
Vinicius <vinicunha2008@gmail.com>
2022-02-11 10:05:57 -0300

    products controllers

--- src/middlewares/validSchema.js
+++ src/middlewares/validSchema.js
@@ -1,12 +1,12 @@
-import joi from'joi';
+import joi from 'joi';
 
-export default function validSchema(schema){
-  return function(req, res, next) {
-      const validation = schema.validate(req.body);
+export default function validSchema(schema) {
+  return function (req, res, next) {
+    const validation = schema.validate(req.body);
 
-      if (validation.error) {
-        return res.sendStatus(422);
-      }
+    if (validation.error) {
+      return res.status(422).send(validation.error.details.map(obj => (obj.message)));
+    }
     next();
   }
 }
