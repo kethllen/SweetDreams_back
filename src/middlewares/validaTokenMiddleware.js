eb4eb6eaab2652b7d2b708de14aac6cdac3f6f5b
kethllen <kety_andrey@hotmail.com>
2022-02-11 14:16:43 -0300

    headers msg

--- src/middlewares/validaTokenMiddleware.js
+++ src/middlewares/validaTokenMiddleware.js
@@ -5,20 +5,22 @@ export default async function validateToken(req, res, next) {
   try {
     const { Authorization } = req.headers;
     const token = Authorization?.replace("Bearer ", "");
+    console.log(token);
     if (!token) {
-      return res.sendStatus(401);
+      return res.status(401).send({ message: "token vazio" });
     }
 
     const session = await db.collection("sessions").findOne({ token });
     if (!session) {
-      return res.sendStatus(401);
+      return res.status(401).send({ message: "sessao nao encontrada" });
     }
 
     const participant = await db
       .collection("users")
-      .findOne({ _id: new ObjectId(session.userId) });
+      .findOne({ _id: session.userId });
     if (!participant) {
-      return res.sendStatus(401);
+      console.log("deu ruim no usuario");
+      return res.status(401).send({ message: "usuario nao encontrado" });
     }
 
     res.locals.user = participant;
