dd8c5794dda559eb56e384b13512082dd27c8576
kethllen <kety_andrey@hotmail.com>
2022-02-11 13:31:21 -0300

    get cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -2,38 +2,71 @@ import { ObjectId } from "mongodb";
 import db from "../db.js";
 
 export async function postItemOnCart(req, res) {
-    try {
-        const productId = req.body.productId;
+  try {
+    const productId = req.body.productId;
 
-        const { user } = res.locals;
-        const userCart = await db.collection('cart').findOne({ id_user: new ObjectId(user._id) });
+    const { user } = res.locals;
+    const userCart = await db
+      .collection("cart")
+      .findOne({ id_user: new ObjectId(user._id) });
 
-        let alreadyOnCart = false;
-        for (let i = 0; i < userCart.cart.length; i++) {
-            if (userCart.cart[i].productId === productId) {
-                alreadyOnCart = true;
-            }
-        }
-
-        if (!alreadyOnCart) {
-            await db.collection('cart').updateOne({
-                id_user: new ObjectId(user._id)
-            }, {
-                $push: { cart: { _id: new ObjectId(), productId, quantity: 1 } }
-            });
-        } else {
-            await db.collection('cart').updateOne({
-                id_user: new ObjectId(user._id),
-                "cart.productId": productId
-            }, {
-                $inc: { "cart.$.quantity": 1 }
-            });
-        }
-        const updatedCart = await db.collection('cart').findOne({ id_user: new ObjectId(user._id) });
-
-        res.status(201).send(updatedCart);
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
+    let alreadyOnCart = false;
+    for (let i = 0; i < userCart.cart.length; i++) {
+      if (userCart.cart[i].productId === productId) {
+        alreadyOnCart = true;
+      }
     }
+
+    if (!alreadyOnCart) {
+      await db.collection("cart").updateOne(
+        {
+          id_user: new ObjectId(user._id),
+        },
+        {
+          $push: { cart: { _id: new ObjectId(), productId, quantity: 1 } },
+        }
+      );
+    } else {
+      await db.collection("cart").updateOne(
+        {
+          id_user: new ObjectId(user._id),
+          "cart.productId": productId,
+        },
+        {
+          $inc: { "cart.$.quantity": 1 },
+        }
+      );
+    }
+    const updatedCart = await db
+      .collection("cart")
+      .findOne({ id_user: new ObjectId(user._id) });
+
+    res.status(201).send(updatedCart);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function getCart(req, res) {
+  try {
+    let productsInfo = [];
+    const { user } = res.locals;
+    const userCart = await db.collection("cart").findOne({ id_user: user._id });
+    for (let product of userCart.cart) {
+      const info = await db
+        .collection("products")
+        .findOne({ _id: product.productId });
+      productsInfo.push({
+        name: info.name,
+        image: info.image,
+        price: info.price,
+      });
+    }
+
+    res.status(200).send(productsInfo);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,10 +1,15 @@
-import { Router } from 'express';
-import { postItemOnCart } from '../controllers/cartController.js';
-import validSchema from '../middlewares/validSchema.js';
-import cartSchema from '../schemas/cartSchema.js';
-import validaTokenMiddleware from '../middlewares/validaTokenMiddleware.js';
+import { Router } from "express";
+import { postItemOnCart, getCart } from "../controllers/cartController.js";
+import validSchema from "../middlewares/validSchema.js";
+import cartSchema from "../schemas/cartSchema.js";
+import validaTokenMiddleware from "../middlewares/validaTokenMiddleware.js";
 
 const cartRouter = Router();
-cartRouter.post("/cart", validSchema(cartSchema), validaTokenMiddleware, postItemOnCart);
-
+cartRouter.post(
+  "/cart",
+  validSchema(cartSchema),
+  validaTokenMiddleware,
+  postItemOnCart
+);
+cartRouter.get("/cart", validaTokenMiddleware, getCart);
 export default cartRouter;
