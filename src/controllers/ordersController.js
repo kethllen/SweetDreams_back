d7ca6e3bb5cef116dfa48852187c4c6c7af0ecc2
Vinicius <vinicunha2008@gmail.com>
2022-02-14 10:28:17 -0300

    cart deleted

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -5,7 +5,9 @@ export async function postOrder(req, res) {
         const userInfo = req.body;
         const { user } = res.locals;
         const userCart = await db.collection("cart").findOne({ id_user: user._id });
-        await db.collection('orders').insertOne({ id_user: user._id, ...userInfo, cart: userCart.cart });
+        await db.collection("orders").insertOne({ id_user: user._id, ...userInfo, cart: userCart.cart });
+
+        await db.collection("cart").deleteOne({ id_user: user._id });
 
         res.sendStatus(201);
     } catch (error) {
