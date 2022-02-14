a310e84dff13e4507e489e038f590c27faeba878
kethllen <kety_andrey@hotmail.com>
2022-02-14 14:09:50 -0300

    correct bugs

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -97,10 +97,12 @@ export async function updatedCart(req, res) {
       console.log("eu sou o carrinho atual ", userCart.cart);
       if (cart.quantity == 0 || cart.quantity == "0") {
         console.log("entrei no if");
-        carrinho = userCart.cart.filter((product) => {
-          if (product.productId !== new ObjectId(cart.productId))
-            return product;
-        });
+        const items = userCart.cart.toArray();
+        for (let item of items) {
+          if (item.productId !== new ObjectId(cart.productId)) {
+            carrinho.push(item);
+          }
+        }
         await db.collection("cart").updateOne(
           {
             id_user: new ObjectId(user._id),
